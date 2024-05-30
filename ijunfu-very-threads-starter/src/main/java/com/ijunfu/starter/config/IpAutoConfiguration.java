4a32607e3e25a8156bf139c3822de52267f06e55
ijunfu <ijunfu@163.com>
2024-05-30 19:09:10 +0800

    :sparkles: 自定义Starter

--- ijunfu-very-threads-starter/src/main/java/com/ijunfu/starter/config/IpAutoConfiguration.java
+++ ijunfu-very-threads-starter/src/main/java/com/ijunfu/starter/config/IpAutoConfiguration.java
@@ -7,25 +7,52 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * @Title :
+ * @Title : 自动配置类
  * @Author : ijunfu <ijunfu@163.com>
  * @Date : 2024/5/30 15:03
  * @Version: 1.0
  * @Motto : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
  */
-@ComponentScan("com.ijunfu.starter.config")
+@ComponentScan("com.ijunfu.starter.config")     // 扫描自定义配置类
 public class IpAutoConfiguration {
 
+    /**
+     * @Title  : 注册业务类
+     *
+     * @Param	:
+     * @Return : com.ijunfu.starter.service.IpCounterService
+     * @Author : ijunfu <ijunfu@163.com>
+     * @Date   : 2024/5/30 19:02
+     * @Motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
+     */
     @Bean
     public IpCounterService ipCounterService() {
         return new IpCounterServiceImpl();
     }
 
+    /**
+     * @Title  : 注册拦截器类
+     *
+     * @Param	: ipCounterService
+     * @Return : com.ijunfu.starter.interceptor.IpCounterInterceptor
+     * @Author : ijunfu <ijunfu@163.com>
+     * @Date   : 2024/5/30 19:02
+     * @Motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
+     */
     @Bean
     public IpCounterInterceptor interceptor(IpCounterService ipCounterService) {
         return new IpCounterInterceptor(ipCounterService);
     }
 
+    /**
+     * @Title  : 注入拦截器
+     *
+     * @Param	: interceptor
+     * @Return : com.ijunfu.starter.config.SpringMvcConfig
+     * @Author : ijunfu <ijunfu@163.com>
+     * @Date   : 2024/5/30 19:03
+     * @Motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
+     */
     @Bean
     public SpringMvcConfig springMvcConfig(IpCounterInterceptor interceptor) {
         return new SpringMvcConfig(interceptor);
--- ijunfu-very-threads-starter/src/main/java/com/ijunfu/starter/service/IpCounterService.java
+++ ijunfu-very-threads-starter/src/main/java/com/ijunfu/starter/service/IpCounterService.java
@@ -9,6 +9,15 @@ package com.ijunfu.starter.service;
  */
 public interface IpCounterService {
 
+    /**
+     * @Title  : 根据IP记录访问次数
+     *
+     * @Param	:
+     * @Return : void
+     * @Author : ijunfu <ijunfu@163.com>
+     * @Date   : 2024/5/30 19:00
+     * @Motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
+     */
     void record();
 
 }
