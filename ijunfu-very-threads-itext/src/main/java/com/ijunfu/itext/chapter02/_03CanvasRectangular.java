1e4f32fe7f28d747cbcbd88b41866a939c838450
ijunfu <ijunfu@163.com>
2024-06-07 16:48:21 +0800

    :sparkles: iText Canvas Rectangular（矩形）

---
+++ ijunfu-very-threads-itext/src/main/java/com/ijunfu/itext/chapter02/_03CanvasRectangular.java
@@ -0,0 +1,40 @@
+package com.ijunfu.itext.chapter02;
+
+import com.itextpdf.kernel.colors.DeviceCmyk;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @Title  : 使用Canvas绘制矩形：正方形
+ *
+ * @Author : ijunfu <ijunfu@163.com>
+ * @Date   : 2024/6/7 12:14
+ * @Version: 1.0
+ * @Motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
+ *
+ */
+@Slf4j
+public class _03CanvasRectangular {
+
+    public static void main(String[] args) {
+        try(PdfDocument pdfDoc = new PdfDocument(new PdfWriter("E:/itext/ebooks_23.canvasRectangular.pdf"))) {
+
+            PageSize pageSize = PageSize.A4.rotate();
+            PdfPage page = pdfDoc.addNewPage(pageSize);
+
+            PdfCanvas canvas = new PdfCanvas(page);
+
+            canvas.rectangle(pageSize.getWidth()/2-50, pageSize.getHeight()/2-50, 100, 100)
+                    .stroke();
+
+
+        } catch (Exception ex) {
+            log.error("生成pdf文件失败", ex);
+        }
+    }
+}
