061acf4decd5ba4fea92b451e8648fa8711453a3
ijunfu <ijunfu@163.com>
2024-06-08 11:59:24 +0800

    :sparkles: iText 文件合并

--- ijunfu-very-threads-itext/src/main/java/com/ijunfu/itext/chapter05/_02MergePdf.java
+++ ijunfu-very-threads-itext/src/main/java/com/ijunfu/itext/chapter05/_02MergePdf.java
@@ -6,8 +6,8 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.utils.PdfMerger;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -19,6 +19,7 @@ import java.io.IOException;
  * motto  : 简洁的代码是智慧的结晶 卓越的编码是对复杂性的优雅征服
  *
  */
+@Slf4j
 public class _02MergePdf {
 
     public static final String DIR = "E:/itext/";
@@ -56,7 +57,7 @@ public class _02MergePdf {
             document.add(new Paragraph(content));
 
         } catch (Exception ex) {
-            System.err.println(ex);
+            log.error("merge pdf error: {}", ex.getMessage(), ex);
         }
 
     }
