58e60e0cfd371d98ba8f5212baeb7a09a467f96b
ijunfu <ijunfu@163.com>
2024-06-17 20:41:23 +0800

    测试自定义UI

--- web/vue/vue3-plugin/src/main.ts
+++ web/vue/vue3-plugin/src/main.ts
@@ -11,6 +11,9 @@ const app = createApp(App)
 import Button from './plugin/Button'
 app.use(Button)
 
+// 引用自定义插件
+import FuCustomUI from 'fu-custom-ui'
+app.use(FuCustomUI)
 
 app.use(createPinia())
 app.use(router)
--- web/vue/vue3-plugin/src/views/AboutView.vue
+++ web/vue/vue3-plugin/src/views/AboutView.vue
@@ -2,6 +2,9 @@
   <div class="about">
     <h1>This is an about page</h1>
     <MyButton></MyButton>
+    <h2>自定义UI</h2>
+    <fu-button></fu-button>
+    <fu-swiper></fu-swiper>
   </div>
 </template>
 
@@ -11,6 +14,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: flex-start;
   }
 }
 </style>
