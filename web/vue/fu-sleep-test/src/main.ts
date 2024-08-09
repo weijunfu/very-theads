eba021299c22c3c2eef3f3d17194422e97665403
ijunfu <ijunfu@163.com>
2024-08-09 20:02:07 +0800

    动态递增数字

--- web/vue/fu-sleep-test/src/main.ts
+++ web/vue/fu-sleep-test/src/main.ts
@@ -14,6 +14,10 @@ app.use(router)
 import FuSleep from 'fu-sleep'
 app.use(FuSleep)
 
+import FuNumeralIncr from 'fu-numeral-incr'
+app.use(FuNumeralIncr)
+
+
 app.use({
   install(app) {
     app.config.globalProperties.$author = () => {
--- web/vue/fu-sleep-test/src/views/AboutView.vue
+++ web/vue/fu-sleep-test/src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <FuNumeralIncr :count="200" />
+    </div>
+    <div>
+      <FuNumeralIncr :count="400" unit="%" />
+    </div>
+    
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: calc(100vh * 3);
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
   }
 }
 </style>
